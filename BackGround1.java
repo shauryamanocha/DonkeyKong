93c1afc6d994229898d70cc65da2f6c9c91e29d6
shauryamanocha <shaurya.manocha1@gmail.com>
2020-01-09 14:38:11 -0500

    added basic ladder placement

--- BackGround1.java
+++ BackGround1.java
@@ -16,24 +16,42 @@ public class BackGround1 extends World
     public BackGround1()
     {    
         super(600, 600, 1); 
+        addFloors(3,2,3,2,0.3f);
         addObject(new DK(), 200, 150);
         addObject(new Mario(), 125, 500);
-        addFloors(3,2,150);
     }
 
-    private void addFloors(int numFloors, int floorSegmentsPerLevel, int floorSpacing){
+    private void addFloors(int numFloors, int floorSegmentsPerLevel, int ladderLengths, int maxLadders, float ladderProbability){
         for(int i = 1;i<=numFloors;i++){
-            if(i%2 == 0){
-                for(int j = 0;j<floorSegmentsPerLevel;j++){
-                    addObject(new Floor(),getWidth()-j*Floor.getWidth()-Floor.getWidth()/2,getHeight()-floorSpacing*i);
+            int lowestX = Integer.MAX_VALUE,highestX = Integer.MIN_VALUE;
+            int y = getHeight()-Ladder.getHeight()*i*ladderLengths;
+            for(int j = 0;j<floorSegmentsPerLevel;j++){
+                int x = -1;
+                if(i%2 == 0){
+                    x = getWidth()-j*Floor.getWidth()-Floor.getWidth()/2;
+                }else{
+                    x = j*Floor.getWidth()+Floor.getWidth()/2;
                 }
-            }else{
-                for(int j = 0;j<floorSegmentsPerLevel;j++){
-                    addObject(new Floor(),j*Floor.getWidth()+Floor.getWidth()/2,getHeight()-floorSpacing*i);
+                lowestX = Math.min(x,lowestX);
+                highestX = Math.max(x,highestX);
+                addObject(new Floor(),x,y);
+            }
+            lowestX-= Floor.getWidth()/2;
+            highestX+=Floor.getWidth()/2;
+            addLadders(lowestX,highestX,y,ladderLengths,(highestX-lowestX)/maxLadders,ladderProbability);
+
+        }
+        addGroundFloors();
+    }
+
+    private void addLadders(int startX, int endX, int y, int segments, int spacing, float probability){
+        for(int i = 0;i<(endX-startX)/spacing;i++){
+            if(Math.random()<probability){
+                for(int j = -1;j<segments-1;j++){
+                    addObject(new Ladder(),startX+spacing*i,y+Ladder.getHeight()+j*Ladder.getHeight());
                 }
             }
         }
-        addGroundFloors();
     }
 
     private void addGroundFloors(){
---
+++ Ladder.java
@@ -0,0 +1,27 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Ladder here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Ladder extends Actor
+{
+    /**
+     * Act - do whatever the Ladder wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+
+    public static int getHeight(){
+        return new Ladder().getImage().getHeight();   
+    }
+
+    public static int getWidth(){
+        return new Ladder().getImage().getWidth();   
+    }
+}
