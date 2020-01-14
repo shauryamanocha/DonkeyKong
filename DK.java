6386fed95576d21933cb196c3684d122d1f78402
Jack <jack.p.lact@gmail.com>
2020-01-13 22:53:59 -0500

    Donkey Kong Animations
    He can now throw and dance

--- DK.java
+++ DK.java
@@ -2,6 +2,7 @@ import greenfoot.*;
 public class DK extends Actor
 {
     long lastTime;
+    long danceTime;
     public void act() 
     {
         if(isTouching(Mario.class))
@@ -10,10 +11,27 @@ public class DK extends Actor
 
             Greenfoot.stop();
         }
-        if(System.currentTimeMillis() - lastTime > 2500)
+        if(System.currentTimeMillis() - lastTime > 3000)
         {
             lastTime = System.currentTimeMillis();
-            getWorld().addObject(new Barrel(), getX(), getY());
+            setImage("dkThrow.png");
+            getWorld().addObject(new Barrel(), getX()+50, getY());
+        }
+        if(System.currentTimeMillis() - lastTime > 750)
+        {
+                setImage("dkNormal.png");
+        }
+        if((System.currentTimeMillis() - danceTime > 4000 && System.currentTimeMillis() - danceTime < 4250)||System.currentTimeMillis() - danceTime > 4500 && System.currentTimeMillis() - danceTime < 4750)
+        {
+                setImage("dkRightArm.png");
+        }
+        if((System.currentTimeMillis() - danceTime > 4250 && System.currentTimeMillis() - danceTime < 4500)||(System.currentTimeMillis() - danceTime > 4250 && System.currentTimeMillis() - danceTime < 4500))
+        {
+                setImage("dkLeftArm.png");
+        }
+        if(System.currentTimeMillis() - danceTime > 6000)
+        {
+            danceTime = System.currentTimeMillis();
         }
     }    
 }
